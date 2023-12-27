e99b31db7f2b8e99786a2491adf83f336c57a3e4
JustKode <sobu0715@gmail.com>
2023-12-27 23:45:39 +0900

    feat: add some util method

--- src/test/scala/kr/justkode/util/SparkStreamingTestRunner.scala
+++ src/test/scala/kr/justkode/util/SparkStreamingTestRunner.scala
@@ -1,16 +1,25 @@
 package kr.justkode.util
 
+import org.apache.commons.io.FileUtils
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.catalyst.util.DateTimeUtils
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.execution.streaming.sources.MemorySink
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.unsafe.types.UTF8String
+import org.scalatest.flatspec.AnyFlatSpec
+
+import java.io.File
+import java.util.TimeZone
 
 trait SparkStreamingTestRunner extends AnyFlatSpec
-  with Logging
   with BeforeAndAfter
-  with BeforeAndAfterAll {
+  with BeforeAndAfterAll
+  with Logging {
 
   Logger.getLogger("kr.justkode").setLevel(Level.INFO)
 
@@ -20,16 +29,34 @@ trait SparkStreamingTestRunner extends AnyFlatSpec
 
   implicit val ctx = spark.sqlContext
 
+  spark.sparkContext.setLogLevel("WARN")
+
+  val checkpointLocation = "/tmp/spark-structured-streaming-unit-test"
   val logs: MemoryStream[String] = MemoryStream[String]
   val memorySink = new MemorySink
 
   override def beforeAll(): Unit = {
     log.info("************Test Started************")
-
-
+    FileUtils.deleteDirectory(new File(checkpointLocation))
   }
 
-  before {
-    memorySink.clear()
+  override def afterAll(): Unit = {
+    log.info("************Test Ended************")
+    FileUtils.deleteDirectory(new File(checkpointLocation))
+  }
+
+  protected def getDataFrameFromJsonRecordsBySchema(schema: StructType): DataFrame = {
+    logs.toDF()
+      .select(from_json(col("value"), schema) as "data")
+      .select("data.*")
+  }
+
+  protected def getTimestampMs(datetime: String): Long = {
+    val timestampUsec: Long = DateTimeUtils.stringToTimestamp(
+      UTF8String.fromString(datetime),
+      TimeZone.getTimeZone("KST").toZoneId
+    ).get
+
+    timestampUsec / 1000
   }
 }
