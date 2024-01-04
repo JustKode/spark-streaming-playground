aa70c924584e2a45d74db850d426d2af12add598
JustKode <sobu0715@gmail.com>
2024-01-04 23:10:59 +0900

    feat: BasicStreamingTest

--- src/test/scala/kr/justkode/streaming/BasicStreamingTest.scala
+++ src/test/scala/kr/justkode/streaming/BasicStreamingTest.scala
@@ -2,9 +2,11 @@ package kr.justkode.streaming
 
 import kr.justkode.aggregator.BasicAggregator.aggProductPerformanceReport
 import kr.justkode.util.SparkStreamingTestRunner
-import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
-import org.scalatest._
+import org.apache.commons.io.FileUtils
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
+import java.io.File
 
 class BasicStreamingTest extends SparkStreamingTestRunner {
   val schema = StructType(
@@ -16,8 +18,9 @@ class BasicStreamingTest extends SparkStreamingTestRunner {
     )
   )
 
-  before {
+  after {
     logs.reset()
+    FileUtils.deleteDirectory(new File(checkpointLocation))
   }
 
   "result" should "400" in {
@@ -27,6 +30,7 @@ class BasicStreamingTest extends SparkStreamingTestRunner {
     logs.addData("""{"productId":1,"userId":4,"price":100,"timestamp":"2023-12-31T09:00:00.000Z"}""")
 
     val df = getDataFrameFromJsonRecordsBySchema(schema)
+
     val streamingQuery = aggProductPerformanceReport(df)
       .writeStream
       .format("memory")
@@ -38,5 +42,34 @@ class BasicStreamingTest extends SparkStreamingTestRunner {
 
     val result = spark.sql("select * from agg").collectAsList().get(0).getAs[Long]("price")
     assert(result === 400)
+  }
+
+  "test" should "non-exception" in {
+    logs.addData("""{"id": 1, "content": "hello"}""")
+    logs.addData("""{"id": 2, "content": "hello!"}""")
+    logs.addData("""{"id": 3, "content": "hello!!"}""")
+    logs.addData("""{"id": 3, "co""")
+
+    val schema = StructType(
+      Seq(
+        StructField("id", DataTypes.LongType, true),
+        StructField("content", DataTypes.StringType, true)
+      )
+    )
+
+    val df = logs.toDF()
+      .select(from_json(col("value"), schema) as "data")
+      .select("data.*")
+
+    val streamingQuery = df.writeStream
+      .format("memory")
+      .queryName("agg2")
+      .outputMode("append")
+      .start()
+
+    streamingQuery.processAllAvailable()
+
+    val result = spark.sql("select * from agg2").collect()
+    info(rowListToString(result.toSeq))
   }
 }
--- src/test/scala/kr/justkode/util/SparkStreamingTestRunner.scala
+++ src/test/scala/kr/justkode/util/SparkStreamingTestRunner.scala
@@ -2,31 +2,23 @@ package kr.justkode.util
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
-import com.fasterxml.jackson.module.scala.deser.ScalaObjectDeserializerModule
 import org.apache.commons.io.FileUtils
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.internal.Logging
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.catalyst.util.DateTimeUtils
+import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.execution.streaming.sources.MemorySink
-import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.unsafe.types.UTF8String
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
 import org.apache.spark.sql.catalyst.ScalaReflection
 
 import java.io.File
 import java.sql.Timestamp
-import java.util.{Calendar, TimeZone}
+import java.util.Calendar
 
 trait SparkStreamingTestRunner extends AnyFlatSpec
   with BeforeAndAfter
-  with BeforeAndAfterAll
-  with Logging {
-
-  Logger.getLogger("kr.justkode").setLevel(Level.INFO)
+  with BeforeAndAfterAll {
 
   private val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
@@ -44,12 +36,10 @@ trait SparkStreamingTestRunner extends AnyFlatSpec
   val memorySink = new MemorySink
 
   override def beforeAll(): Unit = {
-    log.info("************Test Started************")
     FileUtils.deleteDirectory(new File(checkpointLocation))
   }
 
   override def afterAll(): Unit = {
-    log.info("************Test Ended************")
     FileUtils.deleteDirectory(new File(checkpointLocation))
   }
 
@@ -57,15 +47,6 @@ trait SparkStreamingTestRunner extends AnyFlatSpec
     logs.toDF()
       .select(from_json(col("value"), schema) as "data")
       .select("data.*")
-  }
-
-  protected def getTimestampMs(datetime: String): Long = {
-    val timestampUsec: Long = DateTimeUtils.stringToTimestamp(
-      UTF8String.fromString(datetime),
-      TimeZone.getTimeZone("KST").toZoneId
-    ).get
-
-    timestampUsec / 1000
   }
 
   protected def caseClassObjectToJson[T](obj: T): String = {
@@ -82,5 +63,9 @@ trait SparkStreamingTestRunner extends AnyFlatSpec
 
   protected def calenderToTimestamp(calender: Calendar): Timestamp = {
     new Timestamp(calender.getTimeInMillis / 1000)
+  }
+
+  protected def rowListToString(rows: Seq[Row]): String = {
+    rows.foldLeft("")((x, row) => x + row.toString() + '\n').trim
   }
 }
